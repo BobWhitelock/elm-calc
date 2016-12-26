de25ea756dc2d2ec19463f1701f2ba0287689da8
Bob Whitelock <bob.whitelock1@gmail.com>
2016-12-26 18:50:43 +0000

    Map out basic model/view; no functionality yet

--- src/App.elm
+++ src/App.elm
@@ -1,18 +1,41 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
+import Html exposing (..)
 import Html.Attributes exposing (src)
 
 
 type alias Model =
-    { message : String
-    , logo : String
+    { partialSum : Maybe PartialSum
+    , currentNumber : NumberUnderConstruction
+    }
+
+
+type alias PartialSum =
+    { answer : Float
+    , operator : Maybe Operator
+    }
+
+
+type Operator
+    = Plus
+    | Minus
+    | Multiply
+    | Divide
+
+
+type alias NumberUnderConstruction =
+    { integerPart : List Int
+    , fractionalPart : Maybe (List Int)
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    ( { partialSum = Nothing
+      , currentNumber = NumberUnderConstruction [] Nothing
+      }
+    , Cmd.none
+    )
 
 
 type Msg
@@ -27,9 +50,66 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
+        [ div [] [ result model ]
+        , div
+            []
+            [ numberButton 7
+            , numberButton 8
+            , numberButton 9
+            , operatorButton Divide
+            ]
+        , div
+            []
+            [ numberButton 4
+            , numberButton 5
+            , numberButton 6
+            , operatorButton Multiply
+            ]
+        , div
+            []
+            [ numberButton 1
+            , numberButton 2
+            , numberButton 9
+            , operatorButton Minus
+            ]
+        , div
+            []
+            [ numberButton 0
+            , button [] [ text "." ]
+            , button [] [ text "=" ]
+            , operatorButton Plus
+            ]
         ]
+
+
+result : Model -> Html Msg
+result model =
+    span [] []
+
+
+numberButton : Int -> Html Msg
+numberButton num =
+    button [] [ text (toString num) ]
+
+
+operatorButton : Operator -> Html Msg
+operatorButton operator =
+    let
+        symbol =
+            case operator of
+                Plus ->
+                    "+"
+
+                Minus ->
+                    "−"
+
+                Multiply ->
+                    "×"
+
+                Divide ->
+                    "÷"
+    in
+        button [] [ text symbol ]
 
 
 subscriptions : Model -> Sub Msg
